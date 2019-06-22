f3b929e81cbab12b4ab375b9e528c2ccaec9e956
Nditah <nditah@gmail.com>
2019-06-22 18:35:13 +0100

    Update Merge PMT and PML

--- src/services/env.service.ts
+++ src/services/env.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class EnvService {
   // API_URL = 'https://peacegroup-api.herokuapp.com/api';
-  API_URL = 'http://0.0.0.0:5000/api';
+  API_URL = 'http://172.16.17.39:5000/api';
   
   constructor() { }
 }
